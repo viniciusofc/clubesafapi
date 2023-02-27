9e9e54bfc48efe7ecff57cdace60a20664e105aa
Vinicius Reis <104025315+viniciusofc@users.noreply.github.com>
2023-02-27 14:51:41 -0300

    update 27/02/2023

--- ClubeSaf/src/main/java/com/api/ClubeSaf/repository/filtroParceirosRepository.java
+++ ClubeSaf/src/main/java/com/api/ClubeSaf/repository/filtroParceirosRepository.java
@@ -13,9 +13,8 @@ public interface filtroParceirosRepository extends JpaRepository<filtroParceiros
 	@Query(value = "select * from gr_parceiros par,gr_segmentos seg where  par.id_segmento=seg.id_segmento and par.id_empresa= ?1 and par.cidade= ?2 and par.id_segmento= ?3 ", nativeQuery = true)
 	 public List<filtroParceiros> findByListaAllParceiros(Long idEmp, String cidade, Long idSeg);
 	 
-	 @Query(value = "select * from gr_parceiros par,gr_empresas emp,gr_cidades cid,gr_segmentos seg,zr_cepbr_cidades cidz\r\n"
-	 		+ " where par.id_empresa=emp.id_empresa and par.id_cidade=cid.id_cidade and par.id_segmento=seg.id_segmento and cidz.id_cidade=cid.c_cidade and emp.id_empresa=?1 and upper(par.parceiro) like (%?2%) or\r\n"
-	 		+ " par.id_empresa=emp.id_empresa and par.id_cidade=cid.id_cidade and par.id_segmento=seg.id_segmento and  cidz.id_cidade=cid.c_cidade and emp.id_empresa=?1 and upper(seg.segmento) like (%?2%) or \r\n"
-	 		+ " par.id_empresa=emp.id_empresa and par.id_cidade=cid.id_cidade and par.id_segmento=seg.id_segmento and  cidz.id_cidade=cid.c_cidade and emp.id_empresa=?1 and upper(cidz.cidade) like (%?2%) ", nativeQuery = true)
+	 @Query(value = "select * from gr_parceiros par,gr_empresas emp,gr_segmentos seg where par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(par.parceiro) like (%?2%) "
+	 		+ "or par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(seg.segmento) like (%?2%) "
+	 		+ "or par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(par.cidade) like (%?2%)", nativeQuery = true)
 		 public List<filtroParceiros> findByFiltroGeralParceiros(Long idEmp, String param);
 }
