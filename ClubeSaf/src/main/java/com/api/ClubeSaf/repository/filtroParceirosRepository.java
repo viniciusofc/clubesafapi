f8f6d0e2b03059f7427a78d70add2f2e33b8087e
Vinicius Reis <viniciusreisvr74@gmail.com>
2023-02-28 18:54:43 -0300

    update 28/02/2023

--- ClubeSaf/src/main/java/com/api/ClubeSaf/repository/filtroParceirosRepository.java
+++ ClubeSaf/src/main/java/com/api/ClubeSaf/repository/filtroParceirosRepository.java
@@ -13,8 +13,8 @@ public interface filtroParceirosRepository extends JpaRepository<filtroParceiros
 	@Query(value = "select * from gr_parceiros par,gr_segmentos seg where  par.id_segmento=seg.id_segmento and par.id_empresa= ?1 and par.cidade= ?2 and par.id_segmento= ?3 order by prioridade,parceiro", nativeQuery = true)
 	 public List<filtroParceiros> findByListaAllParceiros(Long idEmp, String cidade, Long idSeg);
 	 
-	 @Query(value = "select * from gr_parceiros par,gr_empresas emp,gr_segmentos seg where par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(par.parceiro) like (%?2%) "
-	 		+ "or par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(seg.segmento) like (%?2%) "
-	 		+ "or par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(par.cidade) like (%?2%)", nativeQuery = true)
+	 @Query(value = "select * from gr_parceiros par,gr_empresas emp,gr_segmentos seg where par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(par.parceiro) like (%?2%)"
+	 		+ "or par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(seg.segmento) like (%?2%)"
+	 		+ "or par.id_empresa=emp.id_empresa and par.id_segmento=seg.id_segmento and emp.id_empresa= ?1 and upper(par.cidade) like (%?2%) order by par.prioridade , par.parceiro", nativeQuery = true)
 		 public List<filtroParceiros> findByFiltroGeralParceiros(Long idEmp, String param);
 }
