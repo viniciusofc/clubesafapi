55521414dc1189e8d0e57adaa6d658bf8ac964a9
Vinicius Reis <viniciusreisvr74@gmail.com>
2023-03-07 11:50:34 -0300

    update 07/03/2023

--- ClubeSaf/src/main/java/com/api/ClubeSaf/model/parcelas.java
+++ ClubeSaf/src/main/java/com/api/ClubeSaf/model/parcelas.java
@@ -1,71 +1,71 @@
-package com.api.ClubeSaf.model;
-
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "er_parcelas")
-public class parcelas {
-
-	@Id
-	@Column(name = "campoid")
-	private Long id;
-	
-	@Column(name = "campocodigodebarras")
-	private String codBarras;
-	
-	@Column(name = "campolinhadigitavel")
-	private String linDigitavel;
-	
-	@Column(name = "campovalor")
-	private Double valor;
-	
-	@Column(name = "campodataven")
-	private String vencimento;
-	
-	@Column(name = "campoid_contrato")
-	private Long inscricao;
-
-	@Column(name = "campoid_uo")
-	private Long iduo;
-
-	public Double getValor() {
-		return valor;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public String getCodBarras() {
-		return codBarras;
-	}
-
-
-	public String getLinDigitavel() {
-		return linDigitavel;
-	}
-
-
-	public String getVencimento() {
-		return vencimento;
-	}
-
-
-	public Long getIdcontrato() {
-		return inscricao;
-	}
-
-	public Long getIduo() {
-		return iduo;
-	}
-
-	public void setIduo(Long iduo) {
-		this.iduo = iduo;
-	}
-	
-}
+//package com.api.ClubeSaf.model;
+//
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "er_parcelas")
+//public class parcelas {
+//
+//	@Id
+//	@Column(name = "campoid")
+//	private Long id;
+//	
+//	@Column(name = "campocodigodebarras")
+//	private String codBarras;
+//	
+//	@Column(name = "campolinhadigitavel")
+//	private String linDigitavel;
+//	
+//	@Column(name = "campovalor")
+//	private Double valor;
+//	
+//	@Column(name = "campodataven")
+//	private String vencimento;
+//	
+//	@Column(name = "campoid_contrato")
+//	private Long inscricao;
+//
+//	@Column(name = "campoid_uo")
+//	private Long iduo;
+//
+//	public Double getValor() {
+//		return valor;
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//
+//	public String getCodBarras() {
+//		return codBarras;
+//	}
+//
+//
+//	public String getLinDigitavel() {
+//		return linDigitavel;
+//	}
+//
+//
+//	public String getVencimento() {
+//		return vencimento;
+//	}
+//
+//
+//	public Long getIdcontrato() {
+//		return inscricao;
+//	}
+//
+//	public Long getIduo() {
+//		return iduo;
+//	}
+//
+//	public void setIduo(Long iduo) {
+//		this.iduo = iduo;
+//	}
+//	
+//}
--- ClubeSaf/src/main/java/com/api/ClubeSaf/service/parcelasService.java
+++ ClubeSaf/src/main/java/com/api/ClubeSaf/service/parcelasService.java
@@ -1,24 +1,24 @@
-package com.api.ClubeSaf.service;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import com.api.ClubeSaf.model.parcelas;
-import com.api.ClubeSaf.repository.parcelasRepository;
-
-
-@Service
-public class parcelasService {
-
-	@Autowired
-	private parcelasRepository parcelasrepository;
-	
-	@Transactional
-	public parcelas buscarCod(String value) {
-		parcelas result = parcelasrepository.findByCodLin(value);
-		return result;
-	}
-	
-	
-}
+//package com.api.ClubeSaf.service;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//import com.api.ClubeSaf.model.parcelas;
+//import com.api.ClubeSaf.repository.parcelasRepository;
+//
+//
+//@Service
+//public class parcelasService {
+//
+//	@Autowired
+//	private parcelasRepository parcelasrepository;
+//	
+//	@Transactional
+//	public parcelas buscarCod(String value) {
+//		parcelas result = parcelasrepository.findByCodLin(value);
+//		return result;
+//	}
+//	
+//	
+//}
