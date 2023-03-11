4fd6f940166008874f41314cace829a5395eace8
Vinicius Reis <104025315+viniciusofc@users.noreply.github.com>
2023-03-11 10:23:57 -0300

    update 11/03/2023 UPPER no SELECT

--- ClubeSaf/src/main/java/com/api/ClubeSaf/repository/cidadeRepository.java
+++ ClubeSaf/src/main/java/com/api/ClubeSaf/repository/cidadeRepository.java
@@ -7,6 +7,6 @@ import com.api.ClubeSaf.model.cidade;
 
 public interface cidadeRepository extends JpaRepository<cidade, Long>{
 	
-	@Query(value = "select ROW_NUMBER() OVER (ORDER BY 0) AS id, cidade from gr_parceiros where id_empresa = ?1 group by cidade", nativeQuery = true)
+	@Query(value = "select ROW_NUMBER() OVER (ORDER BY 0) AS id, UPPER(cidade) as cidade from gr_parceiros where id_empresa = ?1 group by cidade order by cidade ASC", nativeQuery = true)
 	 public List<cidade> findByLista(Long idPlano);
 }
--- ClubeSaf/src/main/java/com/api/ClubeSaf/repository/filtroGeralRepository.java
+++ ClubeSaf/src/main/java/com/api/ClubeSaf/repository/filtroGeralRepository.java
@@ -10,6 +10,6 @@ import com.api.ClubeSaf.model.segmento;
 
 public interface filtroGeralRepository extends JpaRepository<filtroGeral, Long>{
 	
-	@Query(value = "select parceiro as label from gr_parceiros where excluido=false and id_empresa=?1 GROUP BY parceiro union all select s.segmento as label from gr_parceiros p,gr_segmentos s where s.id_segmento=p.id_segmento and p.excluido=false and id_empresa=?1 GROUP BY s.segmento order by label ", nativeQuery = true)
+	@Query(value = "select UPPER(parceiro) as label from gr_parceiros where excluido=false and id_empresa=?1 GROUP BY parceiro union all select UPPER(s.segmento) as label from gr_parceiros p,gr_segmentos s where s.id_segmento=p.id_segmento and p.excluido=false and id_empresa=?1 GROUP BY UPPER(s.segmento) order by label ", nativeQuery = true)
 	 public List<filtroGeral> findByListaAll(Long IdEmpresa);
 }
